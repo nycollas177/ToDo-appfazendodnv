47a623b836a6418c37aee818d274cb7c68cd12cc
nycollas177 <nycollascruz5@gmail.com>
2023-11-29 13:46:58 -0300

    deixando a mudança de tema dinamico

---
+++ public/js/script.js
@@ -0,0 +1,45 @@
+function alterarTema() {
+    const tema = localStorage.getItem("tema")
+    const body = document.querySelector("body")
+    const button = document.querySelector(".tema-button")
+
+    if (tema) {
+        let novoTema
+
+        if (tema === "light") {
+            novoTema = "dark"
+            button.innerHTML= `<img src="/imagens/sun-icon.png" alt="icone do Sol">`
+            body.classList.remove("Light")
+            body.classList.add("dark")
+        } else {
+            novoTema = "light"
+            button.innerHTML= `<img src="/imagens/moon-icon.png" alt="icone do Lua">`
+            body.classList.remove("dark")
+            body.classList.add("Light")
+        }
+
+        localStorage.setItem("tema", novoTema)
+        return
+    }
+
+    localStorage.setItem("tema", "dark")
+    body.classList.add("dark")
+}
+
+function verificarTema() {
+    const tema = localStorage.getItem("tema")
+    const body = document.querySelector("body")
+    const button = document.querySelector(".tema-button")
+
+    if (tema) {
+        if (tema === "dark") {
+            body.classList.add("dark")
+            button.innerHTML= `<img src="/imagens/sun-icon.png" alt="icone do Sol">`
+        } else {
+            body.classList.add("light")
+            button.innerHTML= `<img src="/imagens/moon-icon.png" alt="icone do Lua">`
+        }
+    }
+}
+
+verificarTema()
