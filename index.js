2de48b9f79b3dbe0ba23f4338fe98e810823d8c3
nycollas177 <nycollascruz5@gmail.com>
2023-11-29 16:47:34 -0300

    filtro de tarefas ativas completo

--- index.js
+++ index.js
@@ -71,6 +71,31 @@ app.post('/criar', (requisicao, resposta) => {
     })
 })
 
+app.get('/ativas', (requisicao, resposta) => {
+    const sql = `
+        SELECT * FROM tarefas
+        WHERE completa = 0
+    `
+
+    conexao.query(sql, (erro, dados) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        const tarefas = dados.map((dado) => {
+            return {
+                id: dado.id,
+                descricao: dado.descricao,
+                completa: false
+            }
+        })
+
+        const quantidadeTarefasAtivas = tarefas.length
+
+        resposta.render('ativas', { tarefas, quantidadeTarefas })
+    })
+})
+
 app.get('/', (requisicao, resposta) => {
     const sql = 'SELECT * FROM tarefas'
 
